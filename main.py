ff7a41ac82d06192d4fc4ca3fe0643d2197987bd
Romuilesik <roma.ilesik@gmail.com>
2023-02-26 16:13:12 +0200

    colorama

---
+++ main.py
@@ -0,0 +1,10 @@
+from colorama import init
+init()
+
+from colorama import init
+init()
+from colorama import Fore, Back, Style
+print(Fore.GREEN + 'зеленый текст')
+print(Back.YELLOW + 'на желтом фоне')
+print(Style.BRIGHT + 'стал ярче' + Style.RESET_ALL)
+print('обычный текст')
